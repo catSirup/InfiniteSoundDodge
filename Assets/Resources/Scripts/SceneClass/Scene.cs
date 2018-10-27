6539991e609e193d940e816860605c6cdca7bf41
ChangeWorld <taehyung0116@gmail.com>
2018-10-27 10:39:31 +0900

    Project Upload

---
+++ Assets/Resources/Scripts/SceneClass/Scene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene : MonoBehaviour {
+    public bool b_Running = false;
+    public virtual void Initialize()    { }
+    public virtual void Updated()       { }
+    public virtual void Exit()          { }
+}
